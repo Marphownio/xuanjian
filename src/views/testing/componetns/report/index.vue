759a16ea49e39e9fed9028724719ec12e18a1251
Anthony <1050070285@qq.com>
2023-01-30 21:45:15 +0800

    报告弹窗

---
+++ src/views/testing/componetns/report/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="report">
+    <div>
+      <h4>应用信息</h4>
+      <div class="appContent">
+        <div class="icon">
+          <img :src="appInfo.appIcon" width="100px" height="100px" class="icon-img" />
+        </div>
+        <div class="appInfo">
+          <div class="flex row">
+            <h4>黑产：</h4>
+            <div> {{ appInfo.apkName }}</div>
+          </div>
+          <div class="flex row">
+            <h4>APK MD5：</h4>
+            <div> {{ appInfo.apkMd5 }}</div>
+          </div>
+          <div class="flex row">
+            <h4>上传时间：</h4>
+            <div> {{ appInfo.updateTime }}</div>
+          </div>
+          <div class="flex row">
+            <h4>分析时间：</h4>
+            <div> {{ appInfo.testingTime }} min</div>
+          </div>
+          <div class="flex row">
+            <h4>签名发布者：</h4>
+            <div> {{ appInfo.author }}</div>
+          </div>
+          <div class="flex row">
+            <h4>签名时间：</h4>
+            <div> {{ appInfo.authorTime }}</div>
+          </div>
+          <div class="flex row">
+            <h4>版本号：</h4>
+            <div> {{ appInfo.version }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h4>信息概览</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appInfo: {},
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.appInfo = {
+        appIcon:
+          "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+        apkName: "凯发娱乐",
+        apkMd5: "a60wad89wndwa0dawdwah0d8wahd8aw",
+        updateTime: "2023-01-27 1:47",
+        testingTime: "10",
+        author: "5ad6d6d6d6d",
+        authorTime: "1981-01-02 01:01:02",
+        version: "v0.1.1",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.flex{
+  display: flex;
+}
+.report{
+  padding: 10px;
+}
+.appContent {
+  display: flex;
+  align-items: center;
+}
+.icon {
+  margin: 0 10px;
+}
+.icon-img {
+  border-radius: 20px;
+}
+.appInfo{
+  flex: 1;
+  background-color: #88888850;
+  padding: 10px;
+  border-radius: 10px;
+}
+.row{
+  align-items: center;
+  width: 100%;
+  height: 2rem;
+}
+</style>
--- src/views/testing/index.vue
+++ src/views/testing/index.vue
@@ -1,13 +1,94 @@
 <template>
   <div>
-    <mainContainer> 检测 </mainContainer>
+    <mainContainer>
+      <h1>黑产域名检测入口</h1>
+      <el-steps :active="stepsActive" finish-status="success">
+        <el-step title="上传应用文件"></el-step>
+        <el-step title="">
+          <div slot="title">
+            <div @click="handleShowReport">查看检测报告</div>
+          </div>
+        </el-step>
+      </el-steps>
+      <div class="upload">
+        <h4>
+          我们会根据上传的APK文件，检测应用程序可能搜集隐私，现在请等待检测的APK文件：
+        </h4>
+        <el-upload
+          style="width: 100%"
+          class="upload-demo"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :limit="1"
+          :show-file-list="true"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">请选择一个文件</div>
+        </el-upload>
+      </div>
+    </mainContainer>
+    <el-drawer
+      size="50%"
+      title="黑产检测报告"
+      :visible.sync="drawer"
+      direction="rtl"
+    >
+      <report />
+    </el-drawer>
   </div>
 </template>
 
 <script>
+import report from "./componetns/report";
 export default {
-  name: "Home",
+  components: { report },
+  data() {
+    return {
+      stepsActive: 2,
+      drawer: false,
+    };
+  },
+  methods: {
+    handleAvatarSuccess(e) {
+      console.log("上传成功", e);
+      this.stepsActive = 2;
+    },
+    beforeAvatarUpload(file) {
+      console.log("beforeAvatarUpload", file);
+      const isApk = file.type === "application/vnd.android.package-archive";
+      if (!isApk) {
+        this.$message.error("上传APK文件只能是 APK 格式!");
+      }
+      return isApk;
+    },
+    handleShowReport() {
+      if (this.stepsActive === 2) {
+        this.drawer = true;
+      }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.upload {
+  width: 80%;
+  border: 1px solid black;
+  margin: 10px auto;
+  padding: 10px;
+}
+.upload-demo {
+  width: 100% !important;
+}
+:deep().el-upload-dragger {
+  width: 600px !important;
+  margin: 0 auto;
+}
+:deep().el-upload {
+  margin: 0 auto;
+  display: block;
+}
+</style>

6e2bf59421a7ff5d1b9106d96cc3c4a75a1185f0
Anthony <1050070285@qq.com>
2023-01-30 21:00:38 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import "element-ui/lib/theme-chalk/index.css";
 import App from "./App";
 import router from "./router";
 import mainContainer from "@/components/container";
+import VueParticles from 'vue-particles'
 // 如果想要中文版 element-ui，按如下方式声明
 Vue.use(ElementUI);
+Vue.use(VueParticles)
 Vue.component("mainContainer", mainContainer);
 
 Vue.config.productionTip = false;
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    <mainContainer> 首恶 </mainContainer>
+    <mainContainer>
+      <vue-particles
+        color="#000000"
+      >
+      </vue-particles>
+      <div class="content">
+        <div class="title">
+          黑产应用的后台黑产域名检测
+        </div>
+        <div class="text">
+          利用面向黑产应用后台域名的自动化流量采集技术，财力黑产应用的后台域名，并予以分类
+        </div>
+        <div class="ope">
+          <el-button @click="$router.push({name:'testing'})">开始检测</el-button>
+        </div>
+        <div class="text">统计数据网站过往分析APP个数，检测出黑产域名总个数</div>
+      </div>
+    </mainContainer>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  methods: {
+    push() {},
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.content{
+  position: absolute;
+  top: 40%;
+}
+.title{
+  font-weight: bold;
+  font-size: 2rem;
+}
+.text{
+  padding: 10px 0;
+}
+</style>
